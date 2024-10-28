bfdb92f7eba7bb51fcaaf63af071de830340d6b0
jony <jony.rpm@gmail.com>
2024-10-28 14:40:35 -0300

    actualizado

---
+++ README.md
@@ -0,0 +1,3 @@
+# PROYECTO BACKEND III - ROXANA ABUGAUCH - Primer Preentrega - Adoptame
+
+# Para poder ejecutarlo, hay que clonar el repositorio, iniciar Node e instalar todas las dependecias usadas.
---
+++ src/services/mocking.js
@@ -0,0 +1,43 @@
+import { faker } from "@faker-js/faker";
+import { createHash } from "../utils/index.js";
+
+class MockingService {
+    static async generateMockingUsers(num) {
+        try {
+            const hashedPassword = await createHash("coder123");
+            const users = [];
+
+            for (let i = 0; i < num; i++) {
+                users.push({
+                    first_name: faker.person.firstName(),
+                    last_name: faker.person.lastName(),
+                    email: faker.internet.email(),
+                    password: hashedPassword,
+                    role: faker.helpers.arrayElement(["user", "admin"]),
+                    pets: []
+                });
+            }
+            return users;
+        } catch (error) {
+            console.error("Error generating mock users:", error);
+            throw error;
+        }
+    }
+
+    static async generateMockingPets(num) {
+        const pets = [];
+
+        for (let i = 0; i < num; i++) {
+            pets.push({
+                name: faker.animal.dog(),
+                specie: faker.animal.type(),
+                adopted: false,
+                birthDate: faker.date.past(),
+                image: "https://via.placeholder.com/150"
+            });
+        }
+        return pets;
+    }
+}
+
+export default MockingService;
